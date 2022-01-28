44ec47807a8cd259ad457aef783df17ed654a6a0
Samuel Erdö <e12022482@student.tuwien.ac.at>
2022-01-28 12:52:25 +0100

    init project

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import { Container } from "react-bootstrap";
+
+export default function Main() {
+    return (
+        <Container>
+            <h1>Hallo Welt</h1>
+        </Container>
+    );
+};
