93e31dac0688c3ce8b31ea46d8ff518886df69ba
Samuel Erdö <e12022482@student.tuwien.ac.at>
2022-05-22 15:15:12 +0200

    updated migrations

--- migrations/001-setup.sql
+++ migrations/001-setup.sql
@@ -178,9 +178,9 @@ INSERT INTO PLACES (
 ) 
 VALUES (
     'Caesar',
-    'Wöllersdorf - Kaserne',
-    'Abandoned building near Wöllersdorf.',
-    'https://www.google.com/maps/place/Ehemalige+Babenberger-Kaserne/@47.8650032,16.1858622,387m/data=!3m1!1e3!4m9!1m2!2m1!1skaserne+near+W%C3%B6llersdorfer+Stra%C3%9Fe,+Markt+Piesting!3m5!1s0x476dc9a11dc222fb:0x53888f93afc97dc5!8m2!3d47.8656687!4d16.1878766!15sCjNrYXNlcm5lIG5lYXIgV8O2bGxlcnNkb3JmZXIgU3RyYcOfZSwgTWFya3QgUGllc3RpbmeSAQ1taWxpdGFyeV9iYXNl!5m1!1e4',
+    'Wöllersdorf - Biotop',
+    'Small biotope with an open field in Wöllersdorf.',
+    'https://www.google.com/maps/place/Biotop/@47.8690291,16.1627163,905m/data=!3m1!1e3!4m5!3m4!1s0x476dc84420b3f2e7:0xc9031d1e2acacdae!8m2!3d47.8694726!4d16.1603921',
     NULL,
     '2022-05-12T10:00:00.307Z'
 ), (
@@ -190,11 +190,18 @@ VALUES (
     'https://www.google.com/maps/place/Schloss+Prugg/@48.0290551,16.7756954,3995m/data=!3m1!1e3!4m5!3m4!1s0x476c5b8b6a3f97a9:0x8fe5bf9275480666!8m2!3d48.0257724!4d16.7827474!5m1!1e4',
     NULL,
     '2022-05-12T10:00:00.307Z'
-),(
+), (
     'Caesar',
-    'Pottendorf - Schloss Prugg',
+    'Pottendorf - Schloss',
     'Abandoned castle in Pottendorf.',
     'https://www.google.com/maps/place/Schloss+Pottendorf/@47.9104108,16.3862886,1261m/data=!3m1!1e3!4m13!1m7!3m6!1s0x476c4b300030150b:0x8b9e45d84d862dc0!2s2486+Pottendorf!3b1!8m2!3d47.911426!4d16.3881078!3m4!1s0x476c4b2e472c6733:0xa6415fd6911c3d7d!8m2!3d47.9113498!4d16.3847057!5m1!1e4',
+    NULL,
+    '2022-05-12T10:00:00.307Z'
+), (
+    'Caesar',
+    'Blumau - Ruinen Feld',
+    'Open fields with trees, fences and ruins in Blumau.',
+    'https://www.google.com/maps/place/Werkstra%C3%9Fe+4,+2602+Blumau/@47.9221945,16.3062693,148m/data=!3m1!1e3!4m5!3m4!1s0x476db44f0053029b:0xf1e57695d2c30117!8m2!3d47.9220965!4d16.3063377',
     NULL,
     '2022-05-12T10:00:00.307Z'
 );
